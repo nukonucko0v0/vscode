43f0e17f71a2919a7b748743fc67a392787ba7f1
nukonucko0v0 <s.s891014.1.breakshot@gmail.com>
2018-04-08 15:47:19 +0900

    バックアップ

--- master/levis/app/views/home/index.php
+++ master/levis/app/views/home/index.php
@@ -1,1 +1,39 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="<?php t(MAIN_CHARSET) ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>練習場</title>
+    <link rel="stylesheet" href="/css/common.css">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <div id="container">
+        <header>
+            <h1><a href=""><?php t($_view['h1_ttl']) ?></a></h1>
+            <nav class="global_menu">
+                <ul>
+                    <li><a href="/">準備中</a></li>
+                    <li><a href="/">準備中</a></li>
+                    <li><a href="/">準備中</a></li>
+                    <li><a href="/">準備中</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main class="contet">
+            <section></section>
+        </main>
+        <footer>
+            <nav class="global_menu">
+                <ul>
+                    <li><a href="/">準備中</a></li>
+                    <li><a href="/">準備中</a></li>
+                    <li><a href="/">準備中</a></li>
+                    <li><a href="/">準備中</a></li>
+                </ul>
+            </nav>
+        </footer>
+    </div>
+</body>
+</html>
