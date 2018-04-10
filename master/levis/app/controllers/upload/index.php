a09892f76875587a811b1810e8a98635470c7fec
nukonucko0v0 <s.s891014.1.breakshot@gmail.com>
2018-04-11 01:02:53 +0900

    #upload 特定のファイル判別機能追加

--- master/levis/app/controllers/upload/index.php
+++ master/levis/app/controllers/upload/index.php
@@ -1,19 +1,77 @@
 <?php
+    /**
+     * アップロードされたファイルの判定・操作
+     * @param post $_FILES
+     * @param string $file_category
+     * @return string $reesults
+     *
+     */
+    function upload_files($_files, $file_category) {
+
+        if (is_uploaded_file($_files["upfile"]["tmp_name"])) {
+
+            $file_tmp_name = $_files['upfile']['tmp_name'];
+            $file_name = $_files['upfile']['name'];
+
+            if (pathinfo($file_name, PATHINFO_EXTENSION) != $file_category) {
+                $results = '.' . $file_category . 'ファイルのみ対応しています。';
+            } else {
+                $file_directory = MAIN_APPLICATION_PATH . '/app/files/';
+                if ($file_category == 'csv') {
+                    if (($fp = fopen($_files['upfile']['tmp_name'], "r")) === false) {
+                        //エラー処理
+                    }
+                    // CSVの中身がダブルクオーテーションで囲われていない場合に一文字目が化けるのを回避
+
+                    setlocale(LC_ALL, 'ja_JP');
+                    $i=0;
+                    while (($line = fgetcsv($fp)) !== FALSE) {
+                        mb_convert_variables('UTF-8', 'sjis-win', $line);
+                        if($i == 0){
+                            // タイトル行
+                            $header = $line;
+                            $i++;
+                            continue;
+                        }
+
+                        $data[] = $line;
+
+                        $i++;
+                    }
+                    fclose($fp);
+                    debug($data);
+                }
+
+
+                if (move_uploaded_file($_files["upfile"]["tmp_name"], $file_directory . $_files["upfile"]["name"])) {
+                    chmod($file_directory . $_files["upfile"]["name"], 0644);
+                    $results = $_files["upfile"]["name"] . "をアップロードしました。";
+                } else {
+                    $results = "ファイルをアップロードできません。";
+                }
+            }
+
+        } else {
+           $results = "ファイルが選択されていません。";
+        }
+        return $results;
+    }
+
     $_view['ttl'] = 'ファイルアップロード｜ようこそ！チュートリアルへ';
     $_view['h1_ttl'] = 'ファイルアップロード';
 
-    $file_directory = MAIN_APPLICATION_PATH . '/app/files/';
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //一時ファイルができているか（アップロードされているか）チェック
-        if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
-            if (move_uploaded_file($_FILES["upfile"]["tmp_name"], $file_directory . $_FILES["upfile"]["name"])) {
-                chmod($file_directory . $_FILES["upfile"]["name"], 0644);
-                echo $_FILES["upfile"]["name"] . "をアップロードしました。";
-            } else {
-                echo "ファイルをアップロードできません。";
-            }
-        } else {
-            echo "ファイルが選択されていません。";
+        $file_category = isset($_POST['file_mode']) ? $_POST['file_mode']  : '';
+        switch ($file_category) {
+            case 'txt':
+            case 'csv':
+                $_view['message'] = upload_files($_FILES, $file_category);
+                break;
+            case 'img':
+                break;
+            default:
+                $_view['message'] = "ファイルが選択されていません。";
+                break;
         }
     }
--- master/levis/app/views/upload/index.php
+++ master/levis/app/views/upload/index.php
@@ -2,14 +2,19 @@
 <main>
     <form action="/upload/" method="post" enctype="multipart/form-data">
     <div>
-        <input type="file" name="upfile" id="" class="input_file">
+        <select name="file_mode" id="file_select">
+            <option value="txt">テキストファイル</option>
+            <option value="csv">CSVファイル</option>
+            <option value="img">イメージファイル</option>
+        </select>
     </div>
     <div>
-        <input type="text" name="text" id="" class="input_text">
+        <input type="file" name="upfile" id="" class="input_file">
     </div>
     <div>
         <input type="submit" value="アップロード">
     </div>
     </form>
+    <div><?php h($_view['message']) ?></div>
 </main>
 <?php import('app/views/footer.php') ?>
